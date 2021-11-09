cb610a05e1f515e6d5d63a1595a0ce79c09aedd4
root <root@srv1.abc-server.id>
2021-11-09 09:52:29 +0700

    homepage

---
+++ bookmarks.js
@@ -0,0 +1,33 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "ODOO",
+    links: [
+      { name: "TLN", url: "https://tln.abc-server.id" },
+      { name: "IEG", url: "https://ieg.abc-server.id" },
+      { name: "TMI (Comming Soon)", url: "#" },
+    ],
+  },
+  {
+    title: "WEB",
+    links: [
+      { name: "DAILY DAISY", url: "https://dailydaisy.com" },
+      { name: "CATALOG (Comming Soon)", url:"#"},
+    ],
+  },
+
+  {
+    title: "ANALYTIC",
+    links: [
+      { name: "REPORT", url: "https://bi.abc-server.id" },
+      { name: "DASHBOARD", url: "https://dashboard.abc-server.id" },
+    ],
+  },
+  {
+    title: "APP",
+    links: [
+      { name: "RETOOL", url: "https://app.abc-server.id" },
+      { name: "AIRFLOW", url: "https://etl.abc-server.id"},
+    ],
+  },
+];
